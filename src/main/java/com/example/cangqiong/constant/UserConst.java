87c3368d394aabda5e7db4ac2527b37ba764af52
赵信勇 <89636513+AyongNice@users.noreply.github.com>
2024-07-30 19:52:53 +0800

    Delete src/main/java directory
    重置分支

--- src/main/java/com/example/cangqiong/constant/UserConst.java
+++
@@ -1,9 +0,0 @@
-package com.example.cangqiong.constant;
-
-
-public class UserConst {
-
-    public static final String password = "123456";
-
-    public static final String mimeTypePrefix = "data:image/png;base64,";
-}
